8ab0215929b649eaa2cebcc624ffcb428fd661ed
yasingundogan10 <yasingundogan@posta.mu.edu.tr>
2025-03-07 11:59:38 +0300

    week 3

--- week3/src/FindMax.java
+++ week3/src/FindMax.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public class FindMax {
 
-public class findMax {
+
+    public static void main( String[] args) {
+
+
+    int num1 = Integer.parseInt(args[0]) ;
+    int num2 = Integer.parseInt(args[1]) ;
+
+    boolean condition = num1 > num2;
+    int max = condition ? num1 : num2;
+    System.out.println(max);
+
+
+
+
+
+
+
+
+    }
 }
--- week3/src/FindPrimes.java
+++ week3/src/FindPrimes.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class FindPrimes {
+
+    public static void main(String[] args){
+
+        int number = Integer.parseInt(args[0]);
+
+
+            for ( int i = 2; i <= number; i++){
+                boolean  isPrime = true;
+                for (int j= 2; j < i; j++){
+                    if( i % j == 0 ){
+                        isPrime = false;
+                        break;
+                    }
+
+
+                }
+                if (isPrime) {
+                    System.out.println(i);
+                }
+            }
+
+    }
+
+
 }
